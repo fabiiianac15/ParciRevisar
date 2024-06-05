e2ac98455afb0e488f37fd9703280ab994c1d8f2
fabiiianac15 <fjacostaa@ufpso.edu.co>
2024-06-05 17:26:26 -0500

    parcial

--- src/components/vue/Practica.vue
+++ src/components/vue/Practica.vue
@@ -5,7 +5,7 @@
       <div
         v-for="correo in correos"
         :key="correo.id"
-        class="correo bg-white p-4 flex flex-col justify-between items-start cursor-pointer rounded-lg shadow-md"
+        class="bg-white p-4 flex flex-col items-start cursor-pointer rounded-lg shadow-md"
         :class="{
           'border-4 border-blue-500': correo.selected,
           'border-4 border-blue-300': correo.hovered && !correo.selected
@@ -19,7 +19,7 @@
           <CheckIcon v-if="correo.leido" class="h-6 w-6 text-green-500 mr-2" />
         </div>
         <!-- Resto del contenido de la caja -->
-        <div class="text-left w-full">
+        <div class="w-full">
           <p :class="{ 'font-bold': !correo.leido, 'font-normal': correo.leido }">{{ correo.titulo }}</p>
           <p class="text-gray-500 text-sm">{{ correo.texto }}</p>
           <p class="text-gray-400 text-xs">{{ correo.tiempo }}</p>
@@ -38,13 +38,13 @@
 
 <script setup>
 import { ref } from 'vue';
-import { ExclamationCircleIcon, CheckIcon, XMarkIcon } from '@heroicons/vue/24/solid';
+import { CheckIcon} from '@heroicons/vue/24/solid';
 
 const correos = ref([
-  { id: 'correo1', titulo: 'Correo 1', texto: 'Texto del correo 1', tiempo: 'Hace 1 hora', leido: false, selected: false, hovered: false, emergencia: true, eliminado: false },
-  { id: 'correo2', titulo: 'Correo 2', texto: 'Texto del correo 2', tiempo: 'Hace 1 hora', leido: false, selected: false, hovered: false, emergencia: false, eliminado: false },
-  { id: 'correo3', titulo: 'Correo 3', texto: 'Texto del correo 3', tiempo: 'Hace 2 horas', leido: false, selected: false, hovered: false, emergencia: false, eliminado: true },
-  { id: 'correo4', titulo: 'Correo 4', texto: 'Texto del correo 4', tiempo: 'Ayer', leido: false, selected: false, hovered: false, emergencia: true, eliminado: false },
+  { id: 'correo1', titulo: 'Correo 1', texto: 'Texto del correo 1', tiempo: 'Hace 1 hora', leido: false, selected: false, hovered: false },
+  { id: 'correo2', titulo: 'Correo 2', texto: 'Texto del correo 2', tiempo: 'Hace 1 hora', leido: false, selected: false, hovered: false },
+  { id: 'correo3', titulo: 'Correo 3', texto: 'Texto del correo 3', tiempo: 'Hace 2 horas', leido: false, selected: false, hovered: false },
+  { id: 'correo4', titulo: 'Correo 4', texto: 'Texto del correo 4', tiempo: 'Ayer', leido: false, selected: false, hovered: false },
 ]);
 
 function seleccionarCorreo(correo) {
